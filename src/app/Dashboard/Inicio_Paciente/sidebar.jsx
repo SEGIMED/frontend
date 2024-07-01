433791268bdc8228ae1d35ce8517c5b8cff5025f
Santiago Paz <santipaz19.02@gmail.com>
2024-07-01 18:56:01 -0300

    preconsulta y consulta conectado al backend

--- src/app/Dashboard/Inicio_Paciente/sidebar.jsx
+++ src/app/Dashboard/Inicio_Paciente/sidebar.jsx
@@ -41,8 +41,8 @@ export const SidePte = ({ search, toggleSidebar }) => {
   // Obteniendo el segmento a mostrar
   const segmentToShow = lastSegment.match(/^\d+$/)
     ? pathBeforeLastSegment.substring(
-        pathBeforeLastSegment.lastIndexOf("/") + 1
-      )
+      pathBeforeLastSegment.lastIndexOf("/") + 1
+    )
     : lastSegment;
 
   const dispatch = useAppDispatch();
@@ -91,9 +91,11 @@ export const SidePte = ({ search, toggleSidebar }) => {
   };
 
   const getSchedules = async (headers) => {
+    // const userId = Number(id)
+    const userId = 8
     try {
-      const response = await ApiSegimed.get("/schedules", headers);
-
+      const response = await ApiSegimed.get(`/schedules?patientId=${userId}`, headers);
+      console.log(response.data);
       if (response.data) {
         dispatch(addSchedules(response.data));
       }
