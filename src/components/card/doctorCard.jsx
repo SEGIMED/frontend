e6badb2ea9d053077d75ee32aff5f0052b45a994
Santiago Paz <santipaz19.02@gmail.com>
2024-07-01 18:55:09 -0300

    avatar 2

--- src/components/card/doctorCard.jsx
+++ src/components/card/doctorCard.jsx
@@ -10,12 +10,12 @@ export default function DoctorCard({ doctor, button }) {
     <div className="flex justify-between h-fit border-b border-b-[#cecece] px-6 py-2 md:py-3 items-center">
       <div className="flex gap-3 items-center">
         <div className="md:w-10 md:h-10 w-8 h-8 flex justify-center items-center">
-          {/* <Avatars avatar={doctor.avatar} /> */}
-          <Avatar
+          <Avatars avatar1={doctor?.avatar} />
+          {/* <Avatar
             isBordered
             src={doctor.avatar}
             className="md:w-10 md:h-10 w-8 h-8"
-          />
+          /> */}
         </div>
         <p className="text-start text-[#686868] font-normal text-base leading-6">
           {doctor?.name} {doctor?.lastname}
