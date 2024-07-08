cfdf401b89cd4d84861eb7ddb58c1911b064ac5e
Andre <andre021297@gmail.com>
2024-07-08 14:44:12 -0500

    historial clinico responsive

--- src/components/NavDoc/subNavDoc.jsx
+++ src/components/NavDoc/subNavDoc.jsx
@@ -10,126 +10,145 @@ import IconRegresar from "../icons/iconRegresar";
 import IconArrowDetailDown from "../icons/IconArrowDetailDown";
 import IconArrowDetailUp from "../icons/IconArrowDetailUp";
 
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
 
 export default function SubNavbar({ id }) {
-    const [openDetails, setOpenDetails] = useState(false);
-    const pathname = usePathname();
-    const lastSegment = pathname.substring(pathname.lastIndexOf("/") + 1);
-    console.log(lastSegment);
+  const [openDetails, setOpenDetails] = useState(false);
+  const pathname = usePathname();
+  const lastSegment = pathname.substring(pathname.lastIndexOf("/") + 1);
+  console.log(lastSegment);
 
-    const getLinkClass = (routeLastSegment) => (
-        `/${lastSegment}` === routeLastSegment
-            ? "bg-gray-200" // Estilo para la pestaña activa
-            : " "
-    );
+  const getLinkClass = (routeLastSegment) =>
+    `/${lastSegment}` === routeLastSegment
+      ? "bg-gray-200" // Estilo para la pestaña activa
+      : " ";
 
-    return (
-        <div className="border-b border-b-[#cecece] flex justify-around items-center pr-6">
-            <Navbar
-                className="flex justify-start items-center"
-                classNames={{
-                    item: [
-                        "flex",
-                        "items-center",
-                        "w-full",
-                        "h-full",
-                        "justify-start",
-                        // "px-0",
-                        "pr-4",
-                        "pl-4",
-                        "py-2",
-                        "border-r-1",
-                        "border-l-1",
-                    ],
-                    wrapper: ["px-0",]
-                }}
-            >
-                <NavbarContent className="hidden sm:flex gap-0 px-0">
-                    <NavbarItem className={getLinkClass(rutas.Datos)}>
-                        <Link
-                            className="flex items-center gap-2"
-                            href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}${rutas.Datos}`}
-                            aria-current="page"
-                        >
-                            <IconClinicalHistory /> Datos del Paciente
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem className={getLinkClass(rutas.Consultas)}>
-                        <Link
-                            className="flex items-center gap-2"
-                            href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Consultas}`}
-                            aria-current="page"
-                        >
-                            <IconSubNavbar /> Consultas
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem className={getLinkClass(rutas.Evoluciones)}>
-                        <Link
-                            className="flex items-center gap-2"
-                            href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Evoluciones}`}
-                        >
-                            <IconSubNavbar /> Evoluciones
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem className={getLinkClass(rutas.Anamnesis)}>
-                        <Link
-
-                            className="flex items-center gap-2"
-                            href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Anamnesis}`}
-                        >
-                            <IconSubNavbar /> Anamnesis
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem className={getLinkClass(rutas.Evaluacion)}>
-                        <Link
-                            className="flex items-center gap-2"
-                            href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Evaluacion}`}
-                        >
-                            <IconSubNavbar /> Autoevaluación
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem className="flex items-center gap-2">
-
-                        <Dropdown>
-                            <DropdownTrigger >
-                                <Button style={{
-                                    color: "#686868",
-                                    fontWeight: 400,
-                                    fontSize: 16,
-                                    border: "0",
-                                }}
-                                    variant="bordered"
-                                    onClick={() => setOpenDetails(!openDetails)}
-                                >
-                                    Mas {openDetails ? <IconArrowDetailUp /> : <IconArrowDetailDown />}
-                                </Button>
-                            </DropdownTrigger>
-                            <DropdownMenu aria-label="Static Actions">
-                                <DropdownItem onClick={() => setOpenDetails(!openDetails)} className={getLinkClass(rutas.ExamenFisico)} key="new">     <Link
-                                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.ExamenFisico}`}>
-                                    Examen Fisico
-                                </Link></DropdownItem>
-                                <DropdownItem onClick={() => setOpenDetails(!openDetails)} className={getLinkClass(rutas.SignosVitales)} key="copy"> <Link
-                                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.SignosVitales}`}>
-                                    Signos Vitales
-                                </Link></DropdownItem>
-                                <DropdownItem onClick={() => setOpenDetails(!openDetails)} className={getLinkClass(rutas.Diagnostico)} key="edit">  <Link
-                                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Diagnostico}`}>
-                                    Diagnosticos y tratamientos
-                                </Link></DropdownItem>
-
-                            </DropdownMenu>
-                        </Dropdown>
-
-                    </NavbarItem>
-                </NavbarContent>
-            </Navbar>
-            <Link href={`${rutas.Doctor}${rutas.Pacientes}`}>
-                <button className="flex items-center px-6 py-2 bg-[#487FFA] rounded-xl gap-3 text-white font-bold">
-                    <IconRegresar /> Regresar
-                </button>
+  return (
+    <div className="border-b border-b-[#cecece] flex flex-row-reverse md:flex-row justify-around items-center md:pr-6">
+      <Navbar
+        className="flex justify-start items-center w-[86%] md:w-full"
+        classNames={{
+          item: [
+            "flex",
+            "items-center",
+            "w-full",
+            "h-full",
+            "justify-start",
+            // "px-0",
+            "pr-4",
+            "pl-4",
+            "py-2",
+            "border-r-1",
+            "border-l-1",
+          ],
+          wrapper: ["px-0"],
+        }}>
+        <NavbarContent className="gap-0 px-0 overflow-x-auto">
+          <NavbarItem className={getLinkClass(rutas.Datos)}>
+            <Link
+              className="flex items-center gap-2"
+              href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}${rutas.Datos}`}
+              aria-current="page">
+              <IconClinicalHistory /> Datos del Paciente
             </Link>
-        </div>
-    );
+          </NavbarItem>
+          <NavbarItem className={getLinkClass(rutas.Consultas)}>
+            <Link
+              className="flex items-center gap-2"
+              href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Consultas}`}
+              aria-current="page">
+              <IconSubNavbar /> Consultas
+            </Link>
+          </NavbarItem>
+          <NavbarItem className={getLinkClass(rutas.Evoluciones)}>
+            <Link
+              className="flex items-center gap-2"
+              href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Evoluciones}`}>
+              <IconSubNavbar /> Evoluciones
+            </Link>
+          </NavbarItem>
+          <NavbarItem className={getLinkClass(rutas.Anamnesis)}>
+            <Link
+              className="flex items-center gap-2"
+              href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Anamnesis}`}>
+              <IconSubNavbar /> Anamnesis
+            </Link>
+          </NavbarItem>
+          <NavbarItem className={getLinkClass(rutas.Evaluacion)}>
+            <Link
+              className="flex items-center gap-2"
+              href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Evaluacion}`}>
+              <IconSubNavbar /> Autoevaluación
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="flex items-center gap-2">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  style={{
+                    color: "#686868",
+                    fontWeight: 400,
+                    fontSize: 16,
+                    border: "0",
+                  }}
+                  variant="bordered"
+                  onClick={() => setOpenDetails(!openDetails)}>
+                  Mas{" "}
+                  {openDetails ? (
+                    <IconArrowDetailUp />
+                  ) : (
+                    <IconArrowDetailDown />
+                  )}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem
+                  onClick={() => setOpenDetails(!openDetails)}
+                  className={getLinkClass(rutas.ExamenFisico)}
+                  key="new">
+                  {" "}
+                  <Link
+                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.ExamenFisico}`}>
+                    Examen Fisico
+                  </Link>
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => setOpenDetails(!openDetails)}
+                  className={getLinkClass(rutas.SignosVitales)}
+                  key="copy">
+                  {" "}
+                  <Link
+                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.SignosVitales}`}>
+                    Signos Vitales
+                  </Link>
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => setOpenDetails(!openDetails)}
+                  className={getLinkClass(rutas.Diagnostico)}
+                  key="edit">
+                  {" "}
+                  <Link
+                    href={`${rutas.Doctor}${rutas.Pacientes}${rutas.Historia_Clinica}/${id}/${rutas.Diagnostico}`}>
+                    Diagnosticos y tratamientos
+                  </Link>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+      <Link href={`${rutas.Doctor}${rutas.Pacientes}`}>
+        <button className="flex items-center px-2 md:px-6 py-2 bg-[#487FFA] rounded-xl gap-3 text-white font-bold">
+          <IconRegresar />
+          <p className="hidden md:block">Regresar</p>
+        </button>
+      </Link>
+    </div>
+  );
 }
