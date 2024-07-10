c916e307ce0cce995e7840ad9df7ffe44ab03ce2
Santiago Paz <santipaz19.02@gmail.com>
2024-07-10 12:06:09 -0300

    autoevaluacion

---
+++ src/app/Dashboard/Inicio_Paciente2/Autoevaluacion/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import CuerpoPatient from "@/components/InicioPaciente/autoevaluacion/inputcuerpopatient";
+
+
+
+export default function Autoevaluacion() {
+
+
+    return (
+        <div className="h-full text-[#686868] w-[100%]">
+            <CuerpoPatient />
+        </div>
+    );
+}
