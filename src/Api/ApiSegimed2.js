22c86907d79ca6aa23c73fb54853ffd191e71865
sport <sportvibe07@gmail.com>
2024-07-12 14:41:59 -0400

    cloudinary preconsulta integrado

--- src/Api/ApiSegimed2.js
+++ src/Api/ApiSegimed2.js
@@ -3,5 +3,5 @@ export const url = "https://segimed-backend.onrender.com/api";
 export const local = "http://localhost:5000/api";
 
 export const ApiSegimed2 = axios.create({
-    baseURL: local,
-  });
+  baseURL: url,
+});
