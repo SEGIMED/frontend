f1e7902b14f1fc436a6846957dc438772a228131
Santiago Paz <santipaz19.02@gmail.com>
2024-07-15 19:35:08 -0300

    404 arreglo

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,15 +7,15 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
         <div className="flex flex-col  h-screen bg-gray-100">
-            <div className='pr-3'>
+            <div className='pl-3 h-[10%] justify-center flex md:justify-start'>
                 <LogoSegimed className="w-48 h-20" />
             </div>
-            <div className='flex flex-col items-center justify-center mt-10 gap-6 '>
+            <div className='flex flex-col items-center justify-center  h-[90%] gap-8 md:gap-6 '>
                 <div >
                     <Image404 />
                 </div>
-                <p className='text-bluePrimary leading-6 text-3xl  font-semibold md:text-6xl  '>¡Oops!</p>
-                <p className='text-bluePrimary leading-6 text-center text-3xl  font-semibold md:text-6xl '>No encontramos lo que estás buscando.</p>
+                <p className='text-bluePrimary leading-6 text-6xl  font-semibold md:text-6xl  '>¡Oops!</p>
+                <p className='text-bluePrimary leading-6 text-center text-2xl  font-normal md:text-4xl '>No encontramos lo que estás buscando.</p>
                 <Link href={`/`}>
                     <button className="bg-bluePrimary py-2 px-4 items-center flex rounded-lg gap-2 w-fit ">
                         <p className="block text-white font-bold">
