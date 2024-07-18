c627d665f9400823650b7abfee5f26edc62094b9
Esteban <esteban.aleart@gmail.com>
2024-07-17 22:18:29 -0300

    guardar cambios icon

--- src/app/Dashboard/Inicio_Doctor/Mi_perfil/page.jsx
+++ src/app/Dashboard/Inicio_Doctor/Mi_perfil/page.jsx
@@ -25,6 +25,7 @@ import {
   Button,
 } from "@nextui-org/react";
 import React, { useMemo } from "react";
+import IconUpload from "@/components/icons/IconUpload";
 
 export default function HomeDoc() {
   const dispatch = useAppDispatch();
@@ -171,7 +172,7 @@ export default function HomeDoc() {
                 size={buttonSize}
                 onPress={handleSubmit(onSubmit)}
                 nombre={"Guardar Cambios"}
-                icon={<IconKeep />}
+                icon={<IconUpload/>}
               />
               <Elboton
                 size={buttonSize}
