012174ccffa575b2a1315d1bcd7d0314c400dd04
sport <sportvibe07@gmail.com>
2024-07-19 19:06:57 -0400

    avance estilos preconsulta

--- src/components/preconsulta/PreconsultaSubquestion.jsx
+++ src/components/preconsulta/PreconsultaSubquestion.jsx
@@ -8,31 +8,27 @@ export default function PreconsultaSubquestion({
   subquestionIndex,
   onSubquestionChange,
 }) {
+  const display = section.display ? section.display : '';
   return (
-    <div
-      className={`animate-fade-in flex md:flex-row px-4 md:pl-20 py-4 ${section.options?.length > 0 ? "flex-col" : "flex-row"
-        } `}>
-      <div className="flex items-start md:flex-row w-[50%] gap-2">
+    <div className={`animate-fade-in flex items-start md:flex-row px-4 md:pl-20 py-4`}>
+      {section.title && <div className="flex items-center md:flex-row w-[50%] gap-2">
         <IconCircle className="w-3" />
-        {section.title && <p className="text-[#686868] font-medium text-base leading-4">
-          hola
-        </p>}
-      </div>
+        <p className="text-[#686868] font-normal text-sm leading-4">
+          {section.title}
+        </p>
+      </div>}
       <div
-        className={`${section.options?.length > 0
-          ? "w-[50%] flex flex-row flex-wrap p-4 md:p-0"
-          : "flex-row w-[20%]"
-          } gap-4 items-start `}>
+        className={`w-[50%] flex ${display === 'row' ? 'flex-row' : 'flex-col'} flex-wrap p-4 md:p-0 gap-4 items-start`}>
         {section.options.map((option, index) => (
           <div key={index} className="flex items-start gap-2">
             <input
               type="checkbox"
               id={`checkbox${index}${option.label}`}
-              className="h-6 w-6 cursor-pointer border-gray-300 rounded"
+              className="h-5 w-5 cursor-pointer"
               checked={section.selectedOption === index}
               onChange={() => onSubquestionChange(question, subquestion, index)}
             />
-            <label for={`checkbox${index}${option.label}`} className="text-[#686868] cursor-pointer flex-1 font-medium text-base leading-4">
+            <label htmlFor={`checkbox${index}${option.label}`} className="text-[#686868] cursor-pointer flex-1 font-normal text-sm leading-4">
               {option.label}
             </label>
           </div>
