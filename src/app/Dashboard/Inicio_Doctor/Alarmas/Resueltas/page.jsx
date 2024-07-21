6c23d773ac7ff0d695b348e091b99f040272a2eb
Andre <andre021297@gmail.com>
2024-07-20 19:38:15 -0500

    fix responsive navbar/sidebar both dashboards

--- src/app/Dashboard/Inicio_Doctor/Alarmas/Resueltas/page.jsx
+++ src/app/Dashboard/Inicio_Doctor/Alarmas/Resueltas/page.jsx
@@ -57,7 +57,7 @@ export default function HomeDoc() {
 
   return (
     <div className="h-full flex flex-col overflow-y-auto md:overflow-y-hidden">
-      <div className="flex items-center justify-between border-b border-b-[#cecece] pl-5 pr-6 py-2 bg-white static md:sticky top-0 z-20 md:z-50">
+      <div className="flex items-center justify-between border-b border-b-[#cecece] pl-5 pr-6 py-2 bg-white static md:sticky top-0 z-20 lg:z-50">
         {/* <Ordenar /> */}
         <div></div>
         <h1 className="font-bold">Alarmas resueltas</h1>
@@ -67,7 +67,7 @@ export default function HomeDoc() {
           </button>
         </Link>
       </div>
-      <div className="flex items-center justify-between text-start border-b border-b-[#cecece] py-4 p-2 bg-white static md:sticky top-14 z-20 md:z-50">
+      <div className="flex items-center justify-between text-start border-b border-b-[#cecece] py-4 p-2 bg-white static md:sticky top-14 z-20 lg:z-50">
         <p className="font-bold text-[#5F5F5F] w-1/6">Prioridad</p>
         <p className="font-bold text-[#5F5F5F] w-1/6">Paciente</p>
         <p className="font-bold text-[#5F5F5F] w-1/6">Hora</p>
