cd9b83d6e6fe91e206641009e3f9068a848f146d
Andre <andre021297@gmail.com>
2024-07-23 01:04:47 -0500

    fix styles mi perfil doctor/paciente

--- src/app/Dashboard/Inicio_Paciente/Mi_perfil/page.jsx
+++ src/app/Dashboard/Inicio_Paciente/Mi_perfil/page.jsx
@@ -159,24 +159,24 @@ export default function HomePte() {
       </div>
       <form>
         {edit && (
-          <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-            <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-              <IconCircle className="w-3" />
+          <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+            <label className="w-1/2 flex justify-start gap-3 font-medium">
+              <IconCircle className="w-2" />
               Foto de perfil
             </label>
             <div className="w-1/2 h-8 md:h-12 flex items-center justify-start text-white text-sm font-bold mr-6">
               <Elboton
                 size={buttonSize}
                 onPress={openModalfoto}
-                icon={<IconEditar />}
+                icon2={<IconEditar />}
                 nombre={"Editar Foto"}
               />
             </div>
           </div>
         )}
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Nombre:
           </label>
           {edit ? (
@@ -205,9 +205,9 @@ export default function HomePte() {
             <span className="w-1/2 text-start px-6 py-2">{paciente?.name}</span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Apellido:
           </label>
           {edit ? (
@@ -240,18 +240,18 @@ export default function HomePte() {
             </span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Edad:
           </label>
           <span className="w-1/2 text-start px-6 py-2">
             {CalcularEdad(paciente.sociodemographicDetails?.birthDate)}
           </span>
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Sexo:
           </label>
           {edit ? (
@@ -284,9 +284,9 @@ export default function HomePte() {
             </span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Fecha de nacimiento:
           </label>
           {edit ? (
@@ -313,9 +313,9 @@ export default function HomePte() {
             </span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
             Teléfono de contacto:
           </label>
           {edit ? (
@@ -354,10 +354,10 @@ export default function HomePte() {
             </span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
-            Teléfono de contacto de Emergencia:
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
+            Teléfono de Urgencia:
           </label>
           {edit ? (
             <div className="w-1/2 flex flex-col">
@@ -397,10 +397,10 @@ export default function HomePte() {
             </span>
           )}
         </div>
-        <div className="flex items-center justify-between border-b border-b-[#cecece] pl-3 md:pl-8 py-2">
-          <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-            <IconCircle className="w-3" />
-            Dirección:
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex  justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
+            Lugar de Domicilio:
           </label>
           {edit ? (
             <div className="flex items-center gap-2 w-1/2">
@@ -433,16 +433,31 @@ export default function HomePte() {
             </span>
           )}
         </div>
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
+            Última conexión:
+          </label>
+          <span className="w-1/2 text-start px-6 py-2">
+            {LastLogin(paciente.lastLogin)}
+          </span>
+        </div>
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
+            Actividad Última Semana:
+          </label>
+          <span className="w-1/2 text-start px-6 py-2"></span>
+        </div>
+        <div className="flex items-center justify-between border-b border-b-[#cecece] h-fit md:h-16 pl-3 md:pl-8 py-2">
+          <label className="w-1/2 flex justify-start gap-3 font-medium">
+            <IconCircle className="w-2" />
+            Actividad Última Mes:
+          </label>
+          <span className="w-1/2 text-start px-6 py-2"></span>
+        </div>
       </form>
-      <div className="flex items-center justify-center pl-3 md:pl-8 py-2">
-        <label className="w-1/2 flex items-center justify-start gap-3 font-medium">
-          <IconCircle className="w-3" />
-          Última conexión:
-        </label>
-        <span className="w-1/2 text-start px-6 py-2">
-          {LastLogin(paciente.lastLogin)}
-        </span>
-      </div>
+
       {showModal && (
         <MapModal isOpen={showModal} onClose={() => setShowModal(false)} />
       )}
--- src/components/modal/PhotoModalPTe.jsx
+++ src/components/modal/PhotoModalPTe.jsx
@@ -1,136 +1,152 @@
-"use client"
+"use client";
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import IconClose from "@/components/icons/IconClose";
 import IconCurrentRouteNav from "@/components/icons/IconCurrentRouteNav";
 import IconUpload from "@/components/icons/IconUpload";
-import { useAppSelector, useAppDispatch } from '@/redux/hooks';
-import { adduser } from '@/redux/slices/user/user';
-import Elboton from '@/components/Buttons/Elboton';
-import { ApiSegimed } from '@/Api/ApiSegimed';
-import Cookies from 'js-cookie';
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { adduser } from "@/redux/slices/user/user";
+import Elboton from "@/components/Buttons/Elboton";
+import { ApiSegimed } from "@/Api/ApiSegimed";
+import Cookies from "js-cookie";
+import IconCancel from "../icons/iconCancel";
 
 const PhotoModalPte = ({ isOpen, onClose }) => {
-    const dispatch = useAppDispatch();
-    const fileInputRef = useRef(null);
+  const dispatch = useAppDispatch();
+  const fileInputRef = useRef(null);
 
-    const avatar = useAppSelector(state => state.user.avatar);
-    const user = useAppSelector(state => state.user);
-    const [url, setUrl] = useState(avatar);
-    const [tempUrl, setTempUrl] = useState(url);
+  const avatar = useAppSelector((state) => state.user.avatar);
+  const user = useAppSelector((state) => state.user);
+  const [url, setUrl] = useState(avatar);
+  const [tempUrl, setTempUrl] = useState(url);
 
-    useEffect(() => {
-        setUrl(avatar);
-    }, [avatar]);
+  useEffect(() => {
+    setUrl(avatar);
+  }, [avatar]);
 
-    useEffect(() => {
+  useEffect(() => {
+    setTempUrl(url);
+  }, [url]);
+  useEffect(() => {
+    function onClose2(event) {
+      if (event.key === "Escape") {
         setTempUrl(url);
-    }, [url]);
-    useEffect(() => {
-        function onClose2(event) {
-    
-            if (event.key === 'Escape') {
-                setTempUrl(url)
-                onClose();
-            }
-        }
-    
-        if (typeof window !== "undefined") window.addEventListener("keydown", onClose2);
-        
-        // Cleanup function to remove the event listener when the component unmounts
-        return () => {
-            window.removeEventListener("keydown", onClose2);
-        };
-        }, [onClose]);
-    
-
-
-    const handleClose = () => {
-        setTempUrl(url); // Restablecer URL temporal al valor original
         onClose();
+      }
+    }
+
+    if (typeof window !== "undefined")
+      window.addEventListener("keydown", onClose2);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("keydown", onClose2);
     };
+  }, [onClose]);
 
-    const stopPropagation = (e) => {
-        e.stopPropagation();
-    };
+  const handleClose = () => {
+    setTempUrl(url); // Restablecer URL temporal al valor original
+    onClose();
+  };
 
-    const handleButtonClick = () => {
-        fileInputRef.current.click();
-    };
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
 
-    const handleOnChange = (e) => {
-        try {
-            if (e.target.files.length) {
-                const file = e.target.files[0];
-                const reader = new FileReader();
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
 
-                reader.onload = (event) => {
-                    setTempUrl(event.target.result);
-                };
+  const handleOnChange = (e) => {
+    try {
+      if (e.target.files.length) {
+        const file = e.target.files[0];
+        const reader = new FileReader();
 
-                reader.readAsDataURL(file);
-            }
-        } catch (error) {
-            showNotify('error', 'Error al cargar imagen');
-        }
-    };
-
-    const handleSaveChanges = async () => {
-        setUrl(tempUrl);
-        dispatch(adduser({ ...user, avatar: tempUrl }));
-        const id = Cookies.get("c");
-        const token = Cookies.get("a");
-        const body = {
-            avatar: JSON.stringify(tempUrl),
-            patientId: Number(id)
+        reader.onload = (event) => {
+          setTempUrl(event.target.result);
         };
-        await ApiSegimed.patch(`/update-full-patient`, body, { headers: { 'token': token } });
 
-        onClose();
+        reader.readAsDataURL(file);
+      }
+    } catch (error) {
+      showNotify("error", "Error al cargar imagen");
+    }
+  };
+
+  const handleSaveChanges = async () => {
+    setUrl(tempUrl);
+    dispatch(adduser({ ...user, avatar: tempUrl }));
+    const id = Cookies.get("c");
+    const token = Cookies.get("a");
+    const body = {
+      avatar: JSON.stringify(tempUrl),
+      patientId: Number(id),
     };
+    await ApiSegimed.patch(`/update-full-patient`, body, {
+      headers: { token: token },
+    });
 
-    return isOpen ? (
-        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none" onClick={handleClose}>
-            <div className="fixed inset-0 bg-black opacity-50"></div>
-            <div className="relative z-50 bg-white rounded w-[30rem] h-96 flex flex-col items-center gap-5" onClick={stopPropagation}>
-                <div className="h-full w-full flex flex-col">
-                    <div className="h-16 flex items-center justify-start gap-3 p-5 border-b-2 font-semibold">
-                        <IconCurrentRouteNav className="w-4" /> Editar foto de perfil
-                    </div>
-                    <div className="w-full h-full flex flex-col items-center justify-between p-5">
-                        <div className="flex justify-center items-center">
-                            <div className="w-48 h-48 flex justify-center items-center">
-                                <img
-                                    src={tempUrl}
-                                    alt="Perfil"
-                                    className="w-full h-full object-cover rounded-full"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-col items-center justify-around gap-3">
-                            <Elboton icon={<IconUpload />} nombre={"Subir Foto"} onPress={handleButtonClick} />
-                            <input
-                                type="file"
-                                ref={fileInputRef}
-                                style={{ display: 'none' }}
-                                onChange={handleOnChange}
-                                accept="image/*"
-                            />
-                            <button
-                                onClick={handleSaveChanges}
-                                className="text-[#487FFA] text-sm font-medium"
-                            >
-                                Guardar Cambios
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <button onClick={handleClose} className="absolute top-0 right-0 m-4 hover:transition duration-300 ease-in-out transform hover:scale-105 active:scale-100 active:translate-y-1">
-                    <IconClose className="w-8" />
-                </button>
+    onClose();
+  };
+
+  return isOpen ? (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+      onClick={handleClose}>
+      <div className="fixed inset-0 bg-black opacity-50"></div>
+      <div
+        className="relative z-50 bg-white rounded w-[90%] md:w-[30%]  h-fit flex flex-col items-center gap-5"
+        onClick={stopPropagation}>
+        <div className="h-full w-full flex flex-col">
+          <div className="h-16 flex items-center justify-start gap-3 p-5 border-b-2 font-semibold">
+            <IconCurrentRouteNav className="w-4" /> Editar foto de perfil
+          </div>
+          <div className="w-full h-full flex flex-col items-center justify-between p-5">
+            <div className="flex justify-center items-center">
+              <div className="w-48 h-48 flex justify-center items-center">
+                <img
+                  src={tempUrl}
+                  alt="Perfil"
+                  className="w-full h-full object-cover rounded-full"
+                />
+              </div>
             </div>
+            <div className="flex flex-col items-center justify-around gap-2 w-full">
+              <Elboton
+                icon={<IconUpload />}
+                nombre={"Subir Foto"}
+                onPress={handleButtonClick}
+                className={
+                  "w-full md:w-fit px-6 py-6 md:py-3 md:text-base text-xl"
+                }
+              />
+              <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleOnChange}
+                accept="image/*"
+              />
+              <Elboton
+                nombre={"Eliminar Foto"}
+                onPress={onClose}
+                icon={<IconCancel color="#E73F3F" className="w-6" />}
+                className={
+                  "bg-white text-[#E73F3F] w-full md:w-fit px-6 py-6 md:py-2 md:text-base text-xl"
+                }
+              />
+            </div>
+          </div>
         </div>
-    ) : null;
+        <button
+          onClick={handleClose}
+          className="absolute top-3 md:top-2 right-2 p-2 hover:transition duration-300 ease-in-out transform hover:scale-105 active:scale-100 active:translate-y-1">
+          <IconCancel className="w-8" />
+        </button>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default PhotoModalPte;
