117f212e8654e8f4cb35c8e9ba676e5d9355d929
Santiago Paz <santipaz19.02@gmail.com>
2024-07-25 17:21:20 -0300

    botones igual al pixso riesgo

--- src/components/Buttons/ElBotonDashPte.jsx
+++ src/components/Buttons/ElBotonDashPte.jsx
@@ -1,45 +1,51 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import IconCheckBoton from "../icons/iconCheckBoton";
 
 export default function BotonDashPte({ color, nombre, riesgo }) {
-  let bgColor, textColor, borderColor, iconColor;
+  const [bgColor, setBgColor] = useState("");
+  const [textColor, setTextColor] = useState("");
+  const [borderColor, setBorderColor] = useState("");
+  const [iconColor, setIconColor] = useState("");
 
-  if (nombre === "Bajo" && riesgo === "Bajo") {
-    bgColor = "bg-[#4CAF50]"; // Verde para Bajo
-    textColor = "text-white";
-    borderColor = "border-[#4CAF50]";
-    color = "white";
-  } else if (nombre === "Moderado" && riesgo === "Moderado") {
-    bgColor = "bg-[#FFC900]"; // Amarillo para Medio
-    textColor = "text-white";
-    borderColor = "border-[#FFC900]";
-    color = "white";
-  } else if (nombre === "Alto" && riesgo === "Alto") {
-    bgColor = "bg-[#F44336]"; // Rojo para Alto
-    textColor = "text-white";
-    borderColor = "border-[#F44336]";
-    color = "white";
-  } else if (nombre === "Muy Alto" && riesgo === "Muy Alto") {
-    bgColor = "bg-[#D32F2F]"; // Rojo para Alto
-    textColor = "text-white";
-    borderColor = "border-[#D32F2F]";
-    color = "white";
-  } else {
-    bgColor = "bg-white"; // Fondo blanco por defecto
-    textColor = "text-black";
-    borderColor = "border-gray-300";
-    if (nombre == "Bajo") iconColor = "#70C247";
-    if (nombre == "Moderado") iconColor = "#FFC900";
-    if (nombre == "Alto") iconColor = "#E73F3F";
-    if (nombre == "Muy Alto") iconColor = "#9E193B";
-  }
+  useEffect(() => {
+    if (nombre === "Bajo" && riesgo === "Bajo") {
+      setBgColor("bg-[#4CAF50]"); // Verde para Bajo
+      setTextColor("text-white");
+      setBorderColor("border-[#4CAF50]");
+      setIconColor("white"); // Ensure icon is white
+    } else if (nombre === "Moderado" && riesgo === "Moderado") {
+      setBgColor("bg-[#FFC900]"); // Amarillo para Medio
+      setTextColor("text-white");
+      setBorderColor("border-[#FFC900]");
+      setIconColor("white"); // Ensure icon is white
+    } else if (nombre == "Alto" && riesgo == "Alto") {
+      setBgColor("bg-[#F44336]"); // Rojo para Alto
+      setTextColor("text-white");
+      setBorderColor("border-[#F44336]");
+      setIconColor("white"); // Ensure icon is white
+    } else if (nombre === "Muy Alto" && riesgo === "Muy Alto") {
+      setBgColor("bg-[#D32F2F]"); // Rojo para Muy Alto
+      setTextColor("text-white");
+      setBorderColor("border-[#D32F2F]");
+      setIconColor("white"); // Ensure icon is white
+    } else {
+      setBgColor("bg-white"); // Fondo blanco por defecto
+      setTextColor("text-[#686868]");
+      setBorderColor("border-gray-300");
+      if (nombre === "Bajo") setIconColor("#70C247");
+      if (nombre === "Moderado") setIconColor("#FFC900");
+      if (nombre === "Alto") setIconColor("#E73F3F");
+      if (nombre === "Muy Alto") setIconColor("#9E193B");
+    }
+  }, [nombre, riesgo]);
 
   return (
     <button
-      className={`px-2 md:px-3 py-2 border-4 rounded-lg ${bgColor} ${textColor} ${borderColor} flex items-center justify-center gap-2 md:w-fit w-full`}>
-      <p>{nombre}</p>
+      className={`px-2 md:px-3 py-2 border-1 rounded-lg ${bgColor} ${textColor} ${borderColor} flex items-center justify-center gap-2 md:w-fit w-full`}>
       <IconCheckBoton color={iconColor} className={"w-6"} />
+      <p className=" font-normal text-base leading-8">{nombre}</p>
     </button>
   );
 }
