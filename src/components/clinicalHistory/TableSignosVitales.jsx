762a9ec2ff251511f3e13c2759b86b0e907d7d5d
Santiago Paz <santipaz19.02@gmail.com>
2024-07-25 17:32:46 -0300

    Update TableSignosVitales.jsx

--- src/components/clinicalHistory/TableSignosVitales.jsx
+++ src/components/clinicalHistory/TableSignosVitales.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState, useEffect } from "react";
 import IconArrowDetailDown from "../icons/IconArrowDetailDown";
 import IconArrowDetailUp from "../icons/IconArrowDetailUp";
