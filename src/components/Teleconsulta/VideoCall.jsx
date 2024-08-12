11f34dc60176463925d35161a3e54ee485bbfd91
Emanuel Dominguez <116045677+ManuD33P@users.noreply.github.com>
2024-08-12 14:52:57 -0300

    Update VideoCall.jsx

--- src/components/Teleconsulta/VideoCall.jsx
+++ src/components/Teleconsulta/VideoCall.jsx
@@ -37,7 +37,9 @@ export default function VideoCall({consultId,Role}){
                         rtcPer.setRemoteDescription(offer)
                         .then(()=> rtcPer.createAsw(consultId))
                         .then((asw) =>{
-                            socket.emit('sendAsw',{id:consultId,asw});
+                                if(!this.state){
+                                    socket.emit('sendAsw',{id:consultId,asw});
+                                }
                         })
                     })
                 }
