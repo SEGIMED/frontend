d7f58d138e740a3e3d2bd11de771a1a1c772a5dd
Santiago Paz <santipaz19.02@gmail.com>
2024-09-06 12:32:20 -0300

    Update healthCenters.js

--- src/utils/healthCenters.js
+++ src/utils/healthCenters.js
@@ -1,6 +1,11 @@
 export function healthCenterSwitch(caseNumber) {
   const switchCases = {
     1: "Centro Gallego",
+    2: "Sanatorio de la Trinidad Quilmes",
+    3: "Sanatorio de la Trinidad Ramos Mejía",
+    4: "Sanatorio Dupuytren",
+    7: "Sanatorio Mater Dei",
+    8: "Hospital Municipal de Rehabilitación Respiratoria María Ferrer"
   };
 
   return switchCases[caseNumber] || "Centro Desconocido";
